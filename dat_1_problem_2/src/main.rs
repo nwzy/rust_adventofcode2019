84e73aeb7e73191ea9ce1364ab6b1faf0b2decd9
Nic <nicolasw.wzy@gmail.com>
2020-10-23 22:59:42 -0700

    first commit

---
+++ dat_1_problem_2/src/main.rs
@@ -0,0 +1,42 @@
+use std::fs;
+use std::time::Instant;
+
+fn main() {
+    let now = Instant::now();
+
+    let data = fs::read_to_string("/Users/nicolaswong/Downloads/advent_of_code/advent_day_1.txt")
+        .expect("Unable to read file");
+
+    let mut table: Vec<i32> = Vec::new();
+
+    for element in data.lines() {
+        if let Ok(elem) = element.parse() {
+            table.push(elem)
+        };
+    }
+
+    let mut fuel_vec: Vec<i32> = Vec::new();
+
+    fn fuel_by_mod(mass_: &i32) -> i32 {
+        let f_mass = *mass_ as f32;
+        let mass = (f_mass / 3_f32) - 2_f32;
+        mass.trunc() as i32
+    }
+
+    fn fuel_recurse(input: i32) -> i32 {
+        let num = fuel_by_mod(&input);
+        match num {
+            n if n <= 0 => 0,
+            _ => num + fuel_recurse(num)
+        }
+    }
+
+    for i in table {
+        fuel_vec.push(fuel_recurse(i));
+    };
+
+    let total_vec: i32 = fuel_vec.iter().sum();
+
+    let elapsed = now.elapsed();
+    print!("Sum of fuel_vec: {}\nProgram took {:?} to run", total_vec, elapsed);
+}
---
+++ day_1_problem_1/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+
+fn main() {
+    let data = fs::read_to_string("/Users/nicolaswong/Downloads/advent_of_code/advent_day_1.txt")
+        .expect("Unable to read file");
+
+    let mut table: Vec<i32> = Vec::new();
+
+    for element in data.lines() {
+        if let Ok(elem) = element.parse() {
+            table.push(elem)
+        };
+    }
+
+    fn fuel_by_mod(mass_: &i32) -> i32 {
+        let f_mass = *mass_ as f32;
+        let mass = (f_mass / 3_f32) - 2_f32;
+        mass.trunc() as i32
+    }
+
+    let total: i32 = table.iter().map(|&x| fuel_by_mod(&x)).sum();
+
+    println!("Sum of required fuel: {}", &total);
+}
